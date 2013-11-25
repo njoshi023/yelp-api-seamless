d38bb1b75512ae66ad6b08a33226aca821299ba2
Nitin Joshi <njoshi023@gmail.com>
2013-11-25 01:09:17 -0500

    Corporate site fix

--- main.js
+++ main.js
@@ -1,64 +1,44 @@
 (function($, window){
-	var restaurants = $("#RestaurantResults").find("a[rel^='VendorName']");
+	var restaurants = $("#RestaurantResults").find("a[rel^='VendorName']"),
+		isCorp = false;
 
 	if (restaurants.length === 0) {
+		// For their corp website
+		restaurants = $("#RestaurantListing").find("a[name = 'vendorLocation']");
+		isCorp = true;
+	}
+
+	if (restaurants.length === 0) {
+		alert("Sorry API is unable to find the restaurants on Seamless");
 		return;
-	}	
-
-	var auth = { 
-	  consumerKey: "7BQorpwcEYvhcplPz7bPjA", 
-	  consumerSecret: "Haj-cF4_gz3eYCP4k6AGWML8Vu8",
-	  accessToken: "54nsx5KN1O6undGlE1qKUhrZPgdlYRQD",
-	  accessTokenSecret: "Ym4Lt4sFZfh4zFXRixbfb56QKUE",
-	  serviceProvider: { 
-	    signatureMethod: "HMAC-SHA1"
-	  }
-	};
-
-	var accessor = {
-	  consumerSecret: auth.consumerSecret,
-	  tokenSecret: auth.accessTokenSecret
-	};
+	}
 
 	restaurants.each(function(index, elem) {
 		fetchYelpResult(elem);
 	});
 
 	function fetchYelpResult(elem){
-		var title = $(elem).text().replace(/\(.*\)/,'').trim();
-		var parameters = [];
-		parameters.push(['term', title]);
-		parameters.push(['location', "new york"]);
-		parameters.push(['oauth_consumer_key', auth.consumerKey]);
-		parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-		parameters.push(['oauth_token', auth.accessToken]);
-		parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+		var title = $(elem).text().replace(/\(.*\)/,'').trim(),
+			// url = "http://localhost:5000/search",
+			url = "http://yelpapi.herokuapp.com/search",
+			parameters = {};
 
-		var message = { 
-		  'action': 'http://api.yelp.com/v2/search',
-		  'method': 'GET',
-		  'parameters': parameters 
-		};
-
-		OAuth.setTimestampAndNonce(message);
-		OAuth.SignatureMethod.sign(message, accessor);
-
-		var parameterMap = OAuth.getParameterMap(message.parameters);
-		parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature);
+		parameters.q = title;
+		parameters.l = "10041";
 
 		$.ajax({
-			'url': message.action,
-			'data': parameterMap,
+			'url': url,
+			'data': parameters,
 			'cache': true,
 			'datatype': "jsonp",
 			success: function(data, textStats, XMLHttpRequest) {
-				displayResult(data, $(elem));
+				displayResult(JSON.parse(data), $(elem));
 			}
 		});
 	}
 
- 	function displayResult(results, $elem) {
-		var bestMatch = results.businesses[0];
+	function displayResult(results, $elem) {
+		var bestMatch = JSON.parse(results[0]).businesses[0];
 		if (bestMatch !== undefined) {
 			var imageUrl = bestMatch.rating_img_url;
 			var link = bestMatch.url;
@@ -67,9 +47,15 @@
 			ratingsHtml += "<img src='"+ imageUrl + "'";
 			ratingsHtml += "</img>";
 			ratingsHtml += "</a>"
-		}z
-		
-		var ratingCell = $elem.closest("tr").find(".rating-count").closest("td");
+		}
+
+		var ratingCell;
+		if (!isCorp){
+			ratingCell = $elem.closest("tr").find(".rating-count").closest("td");
+		}
+		else {
+			ratingCell = $elem.closest("tr").find(".rating");	
+		}
 
 		ratingCell.append(ratingsHtml);
 	}
